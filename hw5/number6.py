970eba7e6628c1943837b9bd28a75487452f5042
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-03-10 12:37:01 -0500

    Rearrange and documentation.

--- hw5/number6.py
+++ hw5/number6.py
@@ -3,24 +3,23 @@ from scipy.linalg import inv
 from numpy import dot
 import pandas as pd
 
-A = np.zeros([15, 15])
-np.fill_diagonal(A, 2)
-
-for i, row in enumerate(A):
-    if i == 0:
-        row[i + 1] = -1
-    elif i == len(A) - 1:
-        row[i - 1] = -1
-    else:
-        row[i + 1] = -1
-        row[i - 1] = -1
-
-x = np.arange(1, 16)
-x_star = ([1, -1] * 7 + [1]) * x
-b = dot(A, x)
-
 
 def cg_update(x, p, g, A):
+    """
+    Update logic behind conjugate gradient method.
+    See page 74 of typed notes.
+    Parameters
+    ----------
+        x - vector for current position
+        p - direction to step
+        g - current gradient
+        A - symmetric positive definite matrix
+    Returns
+    -------
+        x_next - vector with the updated position
+        p_next - vector with the updated direction
+        g_next - vector with the updated gradient
+    """
     h = dot(A, p)
     a = -dot(g.T, p) / dot(h.T, p)
     x_next = x + a * p
@@ -31,6 +30,18 @@ def cg_update(x, p, g, A):
 
 
 def cg_gen(x, A, n=15):
+    """
+    Generator to wrap the update logic for the conjugate gradient method.
+
+    Parameters
+    ----------
+        x - vector with initial guess
+        A - symmetric positive definite matrix
+        n - Maximum number of iterations
+    Returns
+    -------
+        generator whose .next() method contains x, p, and g.
+    """
     g = dot(A, x) - b
     p = -g
     for i in range(n):
@@ -42,9 +53,25 @@ def anorm(v, A):
     return np.sqrt(dot(dot(v.T, A), v))
 
 if __name__ == '__main__':
+    A = np.zeros([15, 15])
+    np.fill_diagonal(A, 2)
+
+    for i, row in enumerate(A):
+        if i == 0:
+            row[i + 1] = -1
+        elif i == len(A) - 1:
+            row[i - 1] = -1
+        else:
+            row[i + 1] = -1
+            row[i - 1] = -1
+
+    x = np.arange(1, 16)
+    x_star = ([1, -1] * 7 + [1]) * x
+    b = dot(A, x)
+
     b = dot(A, x_star)
     gen = cg_gen(b, A)
-    df = pd.DataFrame([x for x in gen], columns=['x', 'p', 'g'])
+    df = pd.DataFrame([v for v in gen], columns=['x', 'p', 'g'])
     denom = anorm(x_star, A)
     df['error'] = df.x.apply(lambda x: anorm(x, A) / denom)
     with open('hw5_part6.txt', 'w') as f:
